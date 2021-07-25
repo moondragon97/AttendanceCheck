3a87964bb9dd63d08bbd79af9889ca5f6c6af5e0
MoonDragon <anznfh97@gmail.com>
2021-07-25 21:04:41 +0900

    practice_04

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-/*require("@babel/core").transform(console.log('Hello'), {
-    presets: ["@babel/preset-env"],
-});*/
-console.log("Hello");
+import express from "express";
+const PORT = 5000;
+const app = express();
+
+const home = (req, res, next) => next();
+
+const homesecond = (req, res) => res.send("Hi!!");
+
+app.get("/", home, homesecond)
+
+app.listen(PORT, console.log(`connect acces, PORT Num : ${PORT}`));
