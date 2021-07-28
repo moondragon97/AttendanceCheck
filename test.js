800a76865448c1d33df4c0ede5058e00cdf08b25
MoonDragon <anznfh97@gmail.com>
2021-07-28 15:35:07 +0900

    practice_05

--- test.js
+++ test.js
@@ -1,21 +1,12 @@
 import express from "express";
+import morgan from "morgan";
+
 const PORT = 5000;
 const app = express();
 
-const home = (req, res, next) => {
-    console.log("home");
-    next();
-}
-const homesecond = (req, res, next) => {
-    console.log("homesecond");
-    next();
-}
-
 const homeHandle = (req, res) => res.send("This position is Homepage");
-const testHandle = (req, res) => res.send("This position is Testpage");
 
-app.use(home, homesecond);
+app.use(morgan('dev'));
 app.get("/", homeHandle);
-app.get("/test", testHandle);
 
 app.listen(PORT, console.log(`connect acces, PORT Num : ${PORT}`));
