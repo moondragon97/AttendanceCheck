d7808a000d0e6e85adb4673033ff2caab6083426
MoonDragon <anznfh97@gmail.com>
2021-07-29 17:30:05 +0900

    practice_06

--- test.js
+++ test.js
@@ -4,9 +4,25 @@ import morgan from "morgan";
 const PORT = 5000;
 const app = express();
 
+app.use(morgan('dev'));
+
+const homeRouter = express.Router();
+
 const homeHandle = (req, res) => res.send("This position is Homepage");
 
-app.use(morgan('dev'));
-app.get("/", homeHandle);
+homeRouter.get("/", homeHandle);
+
+const blogRouter = express.Router();
+
+const blogHandle = (req, res) => res.send("Hi Blog");
+
+blogRouter.get("/", blogHandle);
+
+const bestblogHandle = (req, res) => res.send("This is best blog");
+
+blogRouter.get("/best", bestblogHandle);
+
+app.use("/", homeRouter);
+app.use("/blog", blogRouter);
 
 app.listen(PORT, console.log(`connect acces, PORT Num : ${PORT}`));
