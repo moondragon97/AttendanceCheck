d50305442654404df88060957b4d773c22da9ada
MoonDragon <anznfh97@gmail.com>
2021-09-01 16:35:57 +0900

    gitignore test

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // session 미들웨어를 만듦으로써 브라우저에게 session 텍스트를 부여한다.
 app.use(session({
-    secret: "Secret!",     // 암호화를 위한 것이다. 우선 아무 string값을 넣는다.
+    secret: process.env.COOKIE_SECRET,     // 암호화를 위한 것이다. 우선 아무 string값을 넣는다.
     resave: false,          // 변화가 있든 없든 세션을 매번 저장할지(갱실할지).
     saveUninitialized: false,  // 아무 변화 없는 새로운 session을 계속 저장할지(쌓을지).
     cookie:{
