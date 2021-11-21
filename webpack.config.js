613432ffd0ef5e0fc7a9c54ab15e526820fb621a
MoonDragon <anznfh97@gmail.com>
2021-11-21 23:25:04 +0900

    fixed attendanceTime

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,7 @@ const path = require("path");
 
 module.exports = {
     entry: "./src/client/js/main.js",
-    plugins: [new MiniCssExtractPlugin({
-        filename: "css/styles.css",
-    })],
+    mode: "development",
     output:{
         filename: "js/main.js",
         path: path.resolve(__dirname, "assets"),
@@ -13,18 +11,20 @@ module.exports = {
     },
     module:{
         rules: [{test: /\.js$/,
-        use: {
-            loader: "babel-loader",
-            options: {
-                presets: [["@babel/preset-env", {targets: "defaults"}]],
-            }
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: [["@babel/preset-env", {targets: "defaults"}]],
+                }
+            },
         },
+        {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        }],
     },
-    {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-    }],
-    },
-    mode: "development",
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/styles.css",
+    })],
     watch: true,
 };
